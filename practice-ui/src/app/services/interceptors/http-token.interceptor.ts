b6cf3f8c14dfd0cefb443c358d393c4a83e5d854
AhmedEnnaime <ahmedennaime20@gmail.com>
2024-06-11 09:59:49 +0100

    implemented http interceptor

--- practice-ui/src/app/services/interceptors/http-token.interceptor.ts
+++ practice-ui/src/app/services/interceptors/http-token.interceptor.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { KeycloakService } from '../keycloak.service';
 
 @Injectable()
 export class HttpTokenInterceptor implements HttpInterceptor {
+  constructor(private keycloakService: KeycloakService) {}
 
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = this.keycloakService.keycloak.token;
+    if (token) {
+      const authReq = request.clone({
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      });
+      return next.handle(authReq);
+    }
     return next.handle(request);
   }
 }
